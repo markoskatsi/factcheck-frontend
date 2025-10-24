b2f0f0c52da64a7cec34ddec72b0d189041335b1
markoskatsi <markoskatsi05@gmail.com>
2025-10-24 16:39:21 +0100

    Added API folder for rending claims. Also now using scss.

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -1,5 +1,5 @@
 import { Link, NavLink } from "react-router-dom";
-import "./Header.css";
+import "./Header.scss";
 
 function Header() {
   return (
--- src/components/pages/ContactUs.js
+++
@@ -1,5 +0,0 @@
-
-function ContactUs() {
-  return <div>Contact Us</div>;
-}
-export default ContactUs;
