308d159f11260004d9038c4a9fc6356dc13e51b0
markoskatsi <markoskatsi05@gmail.com>
2025-10-24 14:56:09 +0100

    Added router, routes, route NavLink and Link react-router-dom components

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Layout from "./components/layouts/Layout";
 import Home from "./components/pages/Home";
 import PageNotFound from "./components/pages/404";
@@ -5,12 +7,18 @@ import SignIn from "./components/pages/SignIn";
 import ContactUs from "./components/pages/ContactUs";
 import "./App.css";
 
-
 function App() {
   return (
-    <Layout>
-      <ContactUs />
-    </Layout>
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/contactus" element={<ContactUs />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
   );
 }
 
