7d862c91b0f463e1cc040c76376e523636f6ba19
Markos Katsi <markoskatsi05@gmail.com>
2025-10-29 00:09:55 +0000

    dateUtils folder for handling different dates formatting

--- src/components/entity/ClaimItem.js
+++ src/components/entity/ClaimItem.js
@@ -1,27 +1,13 @@
+import { formatDateTime } from "../utils/dateUtils.jsx";
 import "./ClaimItem.scss";
 
 export function ClaimItem({ claim }) {
-
-  const formatDate = (dateString) => {
-    const date = new Date(dateString);
-    const dateStr = date.toLocaleDateString('en-GB', {
-      day: 'numeric',
-      month: 'short',
-      year: 'numeric'
-    });
-    const timeStr = date.toLocaleTimeString('en-GB', {
-      hour: '2-digit',
-      minute: '2-digit'
-    });
-    return `${dateStr} at ${timeStr}`;
-  };
-  
   return (
     <div className="claimItem">
       <h3>{claim.ClaimTitle}</h3>
       <p>{claim.ClaimDescription}</p>
       <p className="status">Status: {claim.ClaimstatusName}</p>
-      <p>Date Published: {formatDate(claim.ClaimPublished)}</p>
+      <p>Date Published: {formatDateTime(claim.ClaimPublished)}</p>
     </div>
   );
 }
