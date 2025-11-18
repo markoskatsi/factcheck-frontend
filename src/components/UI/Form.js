07c655b66945020af4925b7e5baaa65a8d6c4077
markoskatsi <markoskatsi05@gmail.com>
2025-11-18 18:40:34 +0000

    reusable form item for claim form added. no submit option yet

---
+++ src/components/UI/Form.js
@@ -0,0 +1,19 @@
+import "./Form.scss";
+
+export default function FormItem({ children, label, htmlFor, advice, error }) {
+  // Properties -----------------------------------
+  // Hooks ----------------------------------------
+  // Context --------------------------------------
+  // Methods --------------------------------------
+  // View ------------------------------------------
+  return (
+    <div className="FormItem">
+      <label className="FormLabel" htmlFor={htmlFor}>
+        {label}
+      </label>
+      {advice && <p className="FormAdvice">{advice}</p>}
+      {children}
+      {error && <p className="FormError">{error}</p>}
+    </div>
+  );
+}
--- src/components/pages/CreateClaim.js
+++ src/components/pages/CreateClaim.js
@@ -1,5 +1,12 @@
+import ClaimForm from "../entities/claims/ClaimForm";
+
 const CreateClaim = () => {
   return (
-    <div>Create Claim Page</div>
+  <>
+    <h1>Create a new Claim</h1>
+    <ClaimForm />
+  </>
   );
-}
+};
+
+export default CreateClaim;
