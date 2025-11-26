e27660b0d05e41bd8349be5e7f2f8398d36c4c65
markoskatsi <markoskatsi05@gmail.com>
2025-11-26 00:59:55 +0000

    using hardcoded user object context to login

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import { useState, createContext, useContext } from "react";
 import Layout from "./components/layouts/Layout";
 import Home from "./components/pages/Home";
 import MyClaims from "./components/pages/MyClaims";
@@ -7,23 +7,26 @@ import MyClaimInfo from "./components/pages/MyClaimInfo";
 import CreateClaim from "./components/pages/CreateClaim";
 import PageNotFound from "./components/pages/404";
 import Login from "./components/pages/Login";
-
+import { AuthProvider } from "./components/auth/useAuth.jsx";
 import "./App.scss";
 
 function App() {
+ 
   return (
-    <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/myclaims" element={<MyClaims />} />
-          <Route path="/myclaims/:claimId" element={<MyClaimInfo />} />
-          <Route path="/createclaim" element={<CreateClaim />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-      </Layout>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/myclaims" element={<MyClaims />} />
+            <Route path="/myclaims/:claimId" element={<MyClaimInfo />} />
+            <Route path="/createclaim" element={<CreateClaim />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </Layout>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
--- src/components/layouts/Layout.jsx
+++ src/components/layouts/Layout.jsx
@@ -1,17 +1,19 @@
 import Header from "./Header";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import PropTypes from "prop-types";
 
 import "./Layout.scss";
 
 function Layout(props) {
   return (
     <div className="centerpane">
-      <Header />
+      <Header loggedInUser={props.loggedInUser} />
       <Navbar />
       <main>{props.children}</main>
       <Footer />
     </div>
   );
 }
+
 export default Layout;
