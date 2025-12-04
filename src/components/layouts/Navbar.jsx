a779f01a1b861f6c05edad2054dd8728eac21581
markoskatsi <markoskatsi05@gmail.com>
2025-12-04 15:09:17 +0000

    home tab now shows published claims for all users even if not lgoged in

--- src/components/layouts/Navbar.jsx
+++ src/components/layouts/Navbar.jsx
@@ -9,31 +9,35 @@ function Navbar() {
   const { loggedInUser, logout } = useAuth();
   return (
     <nav>
-      <div className="navItem">
-        <NavLink to="/" className={getLinkStyle}>
-          Home
-        </NavLink>
-      </div>
-      {loggedInUser ? (
-        <>
+      <div className="navCenter">
+        <div className="navItem">
+          <NavLink to="/" className={getLinkStyle}>
+            Home
+          </NavLink>
+        </div>
+        {loggedInUser && (
           <div className="navItem">
             <NavLink to="/myclaims" className={getLinkStyle}>
               My Claims
             </NavLink>
           </div>
+        )}
+      </div>
+      <div className="navRight">
+        {loggedInUser ? (
           <div className="navItem">
-            <NavLink to="/logout" onClick={logout} className={getLinkStyle}>
+            <NavLink to="/logout" onClick={logout}>
               Logout
             </NavLink>
           </div>
-        </>
-      ) : (
-        <div className="navItem">
-          <NavLink to="/login" className={getLinkStyle}>
-            Login
-          </NavLink>
-        </div>
-      )}
+        ) : (
+          <div className="navItem">
+            <NavLink to="/login" className={getLinkStyle}>
+              Login
+            </NavLink>
+          </div>
+        )}
+      </div>
     </nav>
   );
 }
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,8 +1,39 @@
-
+import useLoad from "../api/useLoad.js";
+import { CardContainer, Card } from "../UI/Card.jsx";
+import ClaimItem from "../entities/claims/ClaimItem.jsx";
+import { Link } from "react-router-dom";
+import "./MyClaims.scss";
 
 function Home() {
+  // Inititalisation ---------------------------------------
+  const claimsEndpoint = `/claims`;
+  // State -------------------------------------------------
+  const [claims, , , ] = useLoad(claimsEndpoint);
+  // Context -----------------------------------------------
+  // Methods -----------------------------------------------
+  // View --------------------------------------------------
   return (
-    <div>Welcome to FactCheck</div>
+    <section>
+      <h1>Browse Verified Claims</h1>
+      {claims ? (
+        <CardContainer>
+          {claims
+            .filter((claim) => claim.ClaimClaimstatusID === 3)
+            .map((claim) => (
+              <Link to={`/myclaims/${claim.ClaimID}`} key={claim.ClaimID}>
+                <div className="fixed">
+                  <Card>
+                    <ClaimItem claim={claim} />
+                  </Card>
+                </div>
+              </Link>
+            ))}
+        </CardContainer>
+      ) : (
+        <p>No verified claims available.</p>
+      )}
+    </section>
   );
 }
+
 export default Home;
