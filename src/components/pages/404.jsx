040b73b8665e45c807f3369794b197c045c417e4
Markos Katsi <markoskatsi05@gmail.com>
2026-01-01 20:51:26 +0000

    React bits fuzzytext for 404 page

--- src/components/pages/404.jsx
+++ src/components/pages/404.jsx
@@ -1,4 +1,21 @@
+import FuzzyText from "../UI/FuzzyText";
+import "./404.scss";
+
 function PageNotFound() {
-  return <div>Page Not Found</div>;
+  const hoverIntensity = 0.5;
+  const enableHover = true;
+  const color = "#222";
+  return (
+    <div className="errorMessage">
+      <FuzzyText
+        baseIntensity={0.2}
+        hoverIntensity={hoverIntensity}
+        enableHover={enableHover}
+        color={color}
+      >
+        Page Not Found
+      </FuzzyText>
+    </div>
+  );
 }
 export default PageNotFound;
