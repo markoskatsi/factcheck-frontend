371cb7b213d158e5f6a33ff416ed963f106ad9b2
Markos Katsi <markoskatsi05@gmail.com>
2026-01-05 00:31:45 +0000

    reusable dropzone from react installed and implemented. new upload icon added

---
+++ src/components/UI/Dropzone.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { useDropzone } from "react-dropzone";
+import Icon from "./Icons.jsx";
+import "./Dropzone.scss";
+
+export const Dropzone = ({ onDrop, selectedFile }) => {
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+  return (
+    <div
+      {...getRootProps()}
+      className={`dropzone${isDragActive ? " dropzone--active" : ""}`}
+    >
+      <input {...getInputProps()} />
+      {selectedFile ? (
+        <p>Selected file: {selectedFile.name}</p>
+      ) : (
+        <>
+          <Icon.Upload />
+          <p>Drop file here, or click to select a file</p>
+        </>
+      )}
+    </div>
+  );
+};
