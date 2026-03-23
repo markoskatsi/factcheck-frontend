848012a7e7499729df3abcf91e0702f93da352dc
Markos Katsi <markoskatsi05@gmail.com>
2026-03-23 16:42:46 +0000

    assignment for editors completed

--- src/components/pages/editors/ReviewInfo.jsx
+++ src/components/pages/editors/ReviewInfo.jsx
@@ -4,17 +4,26 @@ import ClaimDetails from "../../entities/claims/ClaimAndSources.jsx";
 import AnnotationDetails from "../../entities/annotations/AnnotationAndEvidence.jsx";
 import { Button, ButtonTray } from "../../UI/Button.jsx";
 import "../submitters/MyClaimInfo.scss";
+import { useAuth } from "../../auth/useAuth.jsx";
+import API from "../../api/API.js";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Spinner } from "../../UI/Spinner.jsx";
 
 const ReviewInfo = () => {
   // Initialisation --------------------------------
+  const { loggedInUserID } = useAuth();
   const { claimId } = useParams();
 
+  const navigate = useNavigate();
+  
   const claimEndpoint = `/claims/${claimId}`;
   const claimSourcesEndpoint = `/sources/claims/${claimId}?orderby=SourceCreated%20desc`;
   const annotationClaimEndpoint = `/annotations/claims/${claimId}`;
 
   // State -----------------------------------------
-  const [claims, , ,] = useLoad(claimEndpoint);
+  const [isLoading, setIsLoading] = useState(false);
+  const [claims, , , reloadClaims] = useLoad(claimEndpoint);
   const [annotations, , ,] = useLoad(annotationClaimEndpoint);
   const [sources, , ,] = useLoad(claimSourcesEndpoint);
 
@@ -25,11 +34,24 @@ const ReviewInfo = () => {
   const annotation = annotations?.[0];
 
   // Handlers --------------------------------------
+  const handleAssignment = async () => {
+    setIsLoading(true);
+    const assignmentResponse = await API.post(`/assignments`, {
+      AssignmentClaimID: claim.ClaimID,
+      AssignmentUserID: loggedInUserID,
+    });
+    await reloadClaims(claimEndpoint);
+    setIsLoading(false);
+    navigate(`/editortaskinfo/${claim.ClaimID}`);
+    return assignmentResponse.isSuccess;
+  };
   // View ------------------------------------------
   if (!claim) return <p>Loading...</p>;
   return (
     <>
+    {isLoading && <Spinner />}
       <div className="claimInfoWrapper">
+        <Button onClick={handleAssignment}>Assign to you</Button>
         <div className="claimLayout">
           <div className="claimMain">
             <h2>Claim</h2>
