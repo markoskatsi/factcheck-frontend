ca6c05a0849d44a2a80aeda86a5066b19a16566b
Markos Katsi <markoskatsi05@gmail.com>
2026-03-27 14:21:49 +0000

    little styling for profile and status on submitted work

--- src/components/pages/shared/Profile.jsx
+++ src/components/pages/shared/Profile.jsx
@@ -1,19 +1,57 @@
 import { useAuth } from "../../auth/useAuth";
+import { Card } from "../../UI/Card.jsx";
+import useLoad from "../../api/useLoad.js";
+import { Button, ButtonTray } from "../../UI/Button.jsx";
+import { useNavigate } from "react-router-dom";
+import "./Profile.scss";
 
 const Profile = () => {
-  const { loggedInUser } = useAuth();
-
+  // Initialisation --------------------------------
+  const navigate = useNavigate();
+  const { loggedInUser, loggedInUserID, logout } = useAuth();
   const user = loggedInUser ? loggedInUser : null;
+  
+  // State -----------------------------------------
+  const [claims, , ,] = useLoad(`/claims/users/${loggedInUserID}`);
+  const [annotations, , ,] = useLoad(`/annotations/users/${loggedInUserID}`);
+  const [verdicts, , ,] = useLoad(
+    `/verdicts/users/${loggedInUserID}?VerdictVerdictstatusID=1`,
+  );
 
+  // Handlers --------------------------------------
+  const handleLogout = () => {
+    logout();
+    navigate("/login");
+  };
+
+  // View ------------------------------------------
   if (!user) return <p>Loading...</p>;
   return (
-    <div>
-      <h1>Profile Page</h1>
-      <p>{user.UserFirstname}</p>
-      <p>{user.UserLastname}</p>
-      <p>{user.UserEmail}</p>
-      <p>{user.UsertypeName}</p>
-    </div>
+    <>
+      <h1>Profile</h1>
+      <Card className="profileCard">
+        <img
+          src={user.UserProfileImage}
+          alt={`${user.UserFirstname} ${user.UserLastname}`}
+        />
+        <p>
+          {user.UserFirstname} {user.UserLastname}
+        </p>
+        <p>{user.UserEmail}</p>
+        <p>{user.UsertypeName}</p>
+        {loggedInUserID === 1 && <p>Claims Submitted: {claims?.length}</p>}
+        {loggedInUserID === 2 && (
+          <p>Annotations Submitted: {annotations?.length}</p>
+        )}
+        {loggedInUserID === 3 && <p>Verdicts Made: {verdicts?.length}</p>}
+        <ButtonTray>
+          <Button variant="secondary">Edit</Button>
+          <Button variant="darkDanger" onClick={handleLogout}>
+            Logout
+          </Button>
+        </ButtonTray>
+      </Card>
+    </>
   );
 };
 
