65c3e4f0c80c8ec667095d8d7bea0c6a93097318
markoskatsi <markoskatsi05@gmail.com>
2026-04-21 15:25:40 +0100

    creating a dispute completed

---
+++ src/components/entities/disputes/DisputeForm.jsx
@@ -0,0 +1,84 @@
+import Form from "../../UI/Form.jsx";
+import useLoad from "../../api/useLoad.js";
+import { Dropdown } from "../../UI/Dropdown.jsx";
+
+const emptyDispute = {
+  DisputeDescription: "",
+  DisputeDisputetypeID: 0,
+  DisputeOutcome: 0,
+  DisputeVerdictID: 0,
+};
+
+export default function DisputeForm({
+  onSubmit,
+  onCancel,
+  initialDispute = emptyDispute,
+}) {
+  // Initialisation --------------------------------
+  const disputeTypesEndpoint = "/disputetypes";
+  const validation = {
+    isValid: {
+      DisputeDescription: (desc) => desc.length > 20,
+      DisputeDisputetypeID: (type) => type !== 0,
+    },
+    errorMessage: {
+      DisputeDescription: "Description should be at least 20 characters long",
+      DisputeDisputetypeID: "Please select a dispute type",
+    },
+  };
+
+  const conformance = ["DisputeDisputetypeID"];
+
+  // State -----------------------------------------
+  const [dispute, errors, handleChange, handleSubmit] = Form.useForm(
+    initialDispute,
+    conformance,
+    validation,
+    (data) => onSubmit({ ...data }),
+    onCancel,
+  );
+
+  const [disputeTypes, , loadingTypesMessage] = useLoad(disputeTypesEndpoint);
+
+  // Handlers --------------------------------------
+
+  // View ------------------------------------------
+  return (
+    <Form
+      onSubmit={handleSubmit}
+      onCancel={onCancel}
+      submitText="Submit Dispute"
+    >
+      <Form.Item
+        label="Dispute description"
+        htmlFor="DisputeDescription"
+        advice="Please enter the dispute description"
+        error={errors.DisputeDescription}
+      >
+        <textarea
+          className="FormInput"
+          name="DisputeDescription"
+          value={dispute.DisputeDescription}
+          onChange={handleChange}
+          rows="7"
+        />
+      </Form.Item>
+      <Form.Item
+        label="Dispute type"
+        htmlFor="DisputeDisputetypeID"
+        advice="Please select the dispute type"
+        error={errors.DisputeDisputetypeID}
+      >
+        <Dropdown
+          list={disputeTypes}
+          value={dispute.DisputeDisputetypeID}
+          name="DisputeDisputetypeID"
+          loadingMessage={loadingTypesMessage}
+          handleChange={handleChange}
+          idField="DisputetypeID"
+          labelField="DisputetypeName"
+        />
+      </Form.Item>
+    </Form>
+  );
+}
--- src/components/entities/verdicts/VerdictAndEvidence.jsx
+++ src/components/entities/verdicts/VerdictAndEvidence.jsx
@@ -5,28 +5,118 @@ import Accordion from "../../UI/Accordion.jsx";
 import useLoad from "../../api/useLoad.js";
 import { useParams } from "react-router-dom";
 import { useAuth } from "../../auth/useAuth.jsx";
-import { Button } from "../../UI/Button.jsx";
+import { useState } from "react";
+import API from "../../api/API.js";
+import { Button, ButtonTray } from "../../UI/Button.jsx";
+import { Modal, useModal } from "../../UI/Modal.jsx";
+import DisputeForm from "../disputes/DisputeForm.jsx";
+import { useNavigate } from "react-router-dom";
+import Icon from "../../UI/Icons.jsx";
 
 export default function VerdictAndEvidence({ verdict, evidences }) {
   const evidenceCount = evidences?.length ?? 0;
   const { claimId } = useParams();
   const { loggedInUserID } = useAuth();
+  const navigate = useNavigate();
 
   const claimEndpoint = `/claims/${claimId}`;
 
-  const [claims, , ,] = useLoad(claimEndpoint);
+  const [claims, , , loadClaims] = useLoad(claimEndpoint);
+  const [showModal, modalContent, modalTitle, openModal, closeModal] =
+    useModal(false);
+
+  const [
+    showConfirmationModal,
+    modalConfirmationContent,
+    modalConfirmationTitle,
+    openConfirmationModal,
+    closeConfirmationModal,
+  ] = useModal(false);
+
   const claim = claims?.[0];
 
+  const handleSubmitDispute = async (dispute) => {
+    const response = await API.post("/disputes", {
+      ...dispute,
+      DisputeVerdictID: verdict.VerdictID,
+      DisputeOutcome: 0,
+    });
+    if (response.isSuccess) {
+      await disputeOpened();
+      closeConfirmationModal();
+    }
+    return response.isSuccess;
+  };
+
+  const disputeOpened = async () => {
+    const response = await API.put(`/claims/${claimId}`, {
+      ...claim,
+      ClaimClaimstatusID: 8,
+    });
+    await loadClaims(claimEndpoint);
+    navigate(`/myclaims/${claimId}`);
+    return response.isSuccess;
+  };
+
+  const openDisputeModal = () => {
+    openModal(
+      <DisputeForm onSubmit={showConfirmation} onCancel={closeModal} />,
+      "Open Dispute",
+    );
+  };
+
+  const showConfirmation = (dispute) => {
+    closeModal();
+    openConfirmationModal(
+      <>
+        <p style={{ color: "red" }}>
+          Opening a dispute MAY restart the verification process.
+        </p>
+        <p>Are you sure you want to continue?</p>
+        <ButtonTray>
+          <Button
+            onClick={() => handleSubmitDispute(dispute)}
+            variant="darkDanger"
+          >
+            <Icon.Tick />
+            Proceed
+          </Button>
+          <Button onClick={closeConfirmationModal}>
+            <Icon.Cross />
+            Cancel
+          </Button>
+        </ButtonTray>
+      </>,
+      "Confirm Dispute",
+    );
+  };
+
   return (
-    <Card className="claim-details-card">
-      {claim?.ClaimClaimstatusID === 5 &&
-        loggedInUserID === claim?.ClaimUserID && (
-          <Button variant="orange">Open Dispute</Button>
-        )}
-      <VerdictItem verdict={verdict} />
-      <Accordion title={`Attached Evidence (${evidenceCount})`}>
-        <EvidencesMap evidences={evidences} />
-      </Accordion>
-    </Card>
+    <>
+      <Modal className="Modal" show={showModal} title={modalTitle}>
+        {modalContent}
+      </Modal>
+
+      <Modal
+        className="Modal"
+        show={showConfirmationModal}
+        title={modalConfirmationTitle}
+      >
+        {modalConfirmationContent}
+      </Modal>
+
+      <Card className="claim-details-card">
+        {claim?.ClaimClaimstatusID === 5 &&
+          loggedInUserID === claim?.ClaimUserID && (
+            <Button onClick={openDisputeModal} variant="orange">
+              Open Dispute
+            </Button>
+          )}
+        <VerdictItem verdict={verdict} />
+        <Accordion title={`Attached Evidence (${evidenceCount})`}>
+          <EvidencesMap evidences={evidences} />
+        </Accordion>
+      </Card>
+    </>
   );
 }
